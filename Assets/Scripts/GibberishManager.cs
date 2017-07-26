32e8dc61bfbaa06188fc01b5edd9965179933931
SAE\mvonwilpert <1004624@student.sae.edu.au>
2017-07-26 21:14:14 +1000

    Text Translation is half done. Must swap english to gibberish.

---
+++ Assets/Scripts/GibberishManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GibberishManager : MonoBehaviour {
+
+	public List<string> englishText;
+	public List<string> uniqueTerms;
+	public List<string> gibberishText;
+
+	// Use this for initialization
+	void Start () {
+		List<string> tempEng = new List<string>(); //Temp list of string
+		foreach (string msg in englishText) //For each element in list
+			tempEng.Add (msg);
+
+		foreach (string msg in tempEng) {
+			string tempMsg = msg.Replace (".", ""); //Add a .Replace for each punctuation used
+			string[] words = tempMsg.Split(' '); //Use String.Split to remove spaces
+			foreach (string word in words) { //For each word in each array, check for duplicates
+				if (!uniqueTerms.Contains (word)) //If word is unique, add to list
+					uniqueTerms.Add (word); //bluffle - bluffel
+			}
+		}
+			
+		for (int i = 0; i < uniqueTerms.Count; i++) {
+			//generate gibberish string
+			gibberishText.Add("ewuovb");
+		}
+
+		//Create a list of strings. This list will contain all the words that have been translated. All other words will stay in gibberish
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
