e02a9047c66350d467f3697ae5570ca06a2bda32
MACLEODN\MacLeodn <1010746@student.sae.edu.au>
2017-08-01 15:38:03 +1000

    don't mind it, its just a duplicate of the letter one script :)

---
+++ Assets/Scripts/LetterTemplate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LetterTemplate : MonoBehaviour
+{
+
+    GibberishManager gibMan;
+    public Text letterOne;
+
+    // Use this for initialization
+    void Start()
+    {
+        gibMan = GameObject.FindGameObjectWithTag("GibberishManager").GetComponent<GibberishManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        letterOne.text = gibMan.gibberishText[0];
+    }
+}
