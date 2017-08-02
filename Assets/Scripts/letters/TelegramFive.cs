ccb68421e087f9887c75ed75c70f17815405d3d4
MACLEODN\MacLeodn <1010746@student.sae.edu.au>
2017-08-02 18:59:35 +1000

    saving progress push so i don't lose my mind if it magically disappears

--- Assets/Scripts/letters/TelegramFive.cs
+++ Assets/Scripts/letters/TelegramFive.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TelegramFive : MonoBehaviour {
+public class TelegramFive : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    GibberishManager gibMan;
+    public Text text1;
+    public Text text2;
+    public Text text3;
+    public Text text4;
+    public Text text5;
+    public Text text6;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        gibMan = GameObject.FindGameObjectWithTag("GibberishManager").GetComponent<GibberishManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        text1.text = gibMan.gibberishText[75];
+        text2.text = gibMan.gibberishText[76];
+        text3.text = gibMan.gibberishText[77];
+        text4.text = gibMan.gibberishText[78];
+        text5.text = gibMan.gibberishText[79];
+        text6.text = gibMan.gibberishText[80];
+
+    }
 }
--- Assets/Scripts/letters/TelegramSeven.cs
+++ Assets/Scripts/letters/TelegramSeven.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TelegramSeven : MonoBehaviour {
+public class TelegramSeven : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    GibberishManager gibMan;
+    public Text text1;
+    public Text text2;
+    public Text text3;
+    public Text text4;
+    public Text text5;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        gibMan = GameObject.FindGameObjectWithTag("GibberishManager").GetComponent<GibberishManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        text1.text = gibMan.gibberishText[118];
+        text2.text = gibMan.gibberishText[119];
+        text3.text = gibMan.gibberishText[120];
+        text4.text = gibMan.gibberishText[121];
+        text5.text = gibMan.gibberishText[122];
+
+    }
 }
